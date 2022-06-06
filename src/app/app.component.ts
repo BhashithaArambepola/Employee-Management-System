06285d36fdec35693a970160f43c1f0d18f54d2c
Bhashitha Arambepola <barambepola@gmail.com>
2022-06-06 16:40:11 +0530

    create table using Angular material and implement pagination,sorting and searching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ViewChild,AfterViewInit } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+
+// import {AfterViewInit} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 
 @Component({
@@ -8,14 +14,44 @@ import { DialogComponent } from './dialog/dialog.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularCRUD';
-constructor(private dialog:MatDialog){
+  displayedColumns: string[] = ['productName', 'category','date', 'freshness','price', 'comment'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+constructor(private dialog:MatDialog , private api : ApiService){
 
 }
+  ngOnInit(): void {
+    this.getAllproduct();
+
+  }
 openDialog() {
   this.dialog.open(DialogComponent, {
    width : '30%'
   });
+}
+getAllproduct(){
+this.api.getproduct().subscribe({
+  next: (res)=>{
+
+    this.dataSource=new MatTableDataSource(res);
+    this.dataSource.paginator=this.paginator;
+    this.dataSource.sort=this.sort;
+  },
+  error:(err)=>{
+    alert("Error while feteching the records")
+  }
+})
+}
+applyFilter(event: Event) {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  if (this.dataSource.paginator) {
+    this.dataSource.paginator.firstPage();
+  }
 }
 }
